545f727b11a54395ce69f476205e360b73664b47
shunkang129 <shunkang_129@live.com>
2021-06-27 15:06:25 +0800

    init

---
+++ script.js
@@ -0,0 +1,21 @@
+window.onload = function() {
+
+    var typed = new Typed(".firstText", {
+
+        strings: ["Welcome to my personal portfolio 🚀", "Feel free to look around", "Keep in touch 😀"],
+        typeSpeed: 50,
+        backSpeed: 75,
+        fadeOut: true,
+        loop: true
+    });
+
+    var typed = new Typed(".endingText", {
+
+        strings: ["Thanks for watching!"],
+        typeSpeed: 50,
+        backSpeed: 75,
+        fadeOut: true,
+        loop: true
+    });
+
+}
